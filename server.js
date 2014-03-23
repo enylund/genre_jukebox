cdc21a32027225a329035037c1f8a520de8fbe46
enylund <eric.nylund@yale.edu>
2014-03-23 12:24:53 -0400

    added new message for local host connection

--- server.js
+++ server.js
@@ -76,4 +76,4 @@ function searchTumblr(item, callback) {
 
 }).listen(Number(process.env.PORT || 5000));
 
-console.log("Server running at http://127.0.0.1:8000/");
+console.log("Server running at http://127.0.0.1:5000/");
